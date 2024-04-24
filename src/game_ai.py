d51ba3079f8267de81d8fd27cf8cd0bea003ff2f
EetuKilpi <eetu.kilpi@outlook.com>
2024-04-24 15:02:27 +0300

    Usage of hashmap in game_ai

--- src/game_ai.py
+++ src/game_ai.py
@@ -7,9 +7,6 @@ import game
 ROWS = 6
 COLUMNS = 7
 
-#Luodaan hajautus taulu
-hash_map = {}
-
 def check_value(area, piece, player1_piece, player2_piece):
     """
     Laskee lähellä olevien nappuloiden arvot
@@ -67,40 +64,30 @@ def board_value(board, piece, player1_piece, player2_piece):
 
     return value
 
-def store(board, best_move):
-    """
-    Talletaa arvon hajautustauluun
-    """
-    board_key = str(board)
-    hash_map[board_key] = best_move
 
-def retrieve(board):
-    """
-    Hakee arvon hajautustaulusta
-    """
-    board_key = str(board)
-    if board_key in hash_map:
-        return hash_map[board_key]
-    return None
-
-def minimax(board, depth, alpha, beta, player, move_count, player1_piece, player2_piece): #pylint: disable=too-many-arguments, too-many-return-statements, too-many-branches
+def minimax(board, depth, alpha, beta, player, move_count, player1_piece, player2_piece, hash_map): #pylint: disable=too-many-arguments, too-many-return-statements, too-many-branches
     """
     Minimax-algoritmi alfa-beeta-karsinnalla
     """
-    best_move = retrieve(board)
-    if best_move is not None:
-        return best_move, board_value(board, player2_piece, player1_piece, player2_piece)
+    if move_count == 42:
+        return None, 0
 
-    if depth == 0 or move_count == 42:
-        if move_count == 42:
-            return None, 0
+    if depth == 0:
         return None, board_value(board, player2_piece, player1_piece, player2_piece)
+    
+    ordered_columns = sorted(game.check_free_spaces(board, COLUMNS), key=lambda x: abs(x - COLUMNS // 2))
 
     if player: #pylint: disable=no-else-return
         value = float("-inf")
         column = random.choice(game.check_free_spaces(board, COLUMNS))
 
-        for i in game.check_free_spaces(board, COLUMNS):
+        board_key = str(board)
+        best_move = hash_map.get(board_key)
+        if best_move is not None:
+            ordered_columns.remove(best_move)
+            ordered_columns.insert(0, best_move)
+
+        for i in ordered_columns:
             row = game.next_free_row(board, ROWS, i)
             copy = [row[:] for row in board]
             game.place_piece(copy, row, i, player2_piece)
@@ -109,7 +96,7 @@ def minimax(board, depth, alpha, beta, player, move_count, player1_piece, player
                 return i, 1000
 
             new_value = minimax(copy, depth - 1, alpha, beta, False,
-                                 move_count + 1, player1_piece, player2_piece)[1]
+                                 move_count + 1, player1_piece, player2_piece, hash_map)[1]
             if new_value > value:
                 value = new_value
                 column = i
@@ -117,14 +104,21 @@ def minimax(board, depth, alpha, beta, player, move_count, player1_piece, player
             alpha = max(value, alpha)
             if alpha >= beta:
                 break
-
+        
+        hash_map[board_key] = column
         return column, value
 
     else:
         value = float("inf")
         column = random.choice(game.check_free_spaces(board, COLUMNS))
 
-        for i in game.check_free_spaces(board, COLUMNS):
+        board_key = str(board)
+        best_move = hash_map.get(board_key)
+        if best_move is not None:
+            ordered_columns.remove(best_move)
+            ordered_columns.insert(0, best_move)
+
+        for i in ordered_columns:
             row = game.next_free_row(board, ROWS, i)
             copy = [row[:] for row in board]
             game.place_piece(copy, row, i, player1_piece)
@@ -133,7 +127,7 @@ def minimax(board, depth, alpha, beta, player, move_count, player1_piece, player
                 return i, -1000
 
             new_value = minimax(copy, depth - 1, alpha, beta, True,
-                                 move_count + 1, player1_piece, player2_piece)[1]
+                                 move_count + 1, player1_piece, player2_piece, hash_map)[1]
             if new_value < value:
                 value = new_value
                 column = i
@@ -141,20 +135,20 @@ def minimax(board, depth, alpha, beta, player, move_count, player1_piece, player
             beta = min(value, beta)
             if alpha >= beta:
                 break
-
+        
+        hash_map[board_key] = column
         return column, value
 
 def iterative_deepening(board, max_depth, aplha, beta, player, move_count, time_limit, player1_piece, player2_piece): #pylint: disable=too-many-arguments, line-too-long
     """
     Iteratiivinen syveneminen minimax-algoritmiin
     """
+    hash_map = {}
     start_time = time.time()
     best_move = None
     for depth in range(1, max_depth+1):
-        move, _ = minimax(board, depth, aplha, beta, player,
-                           move_count, player1_piece, player2_piece)
+        best_move, _ = minimax(board, depth, aplha, beta, player,
+                           move_count, player1_piece, player2_piece, hash_map)
         if time.time() - start_time > time_limit:
             break
-        best_move = move
-    store(board, best_move)
     return best_move
